d4d7a45a9b892c142b0524186b47c43263ccd2c5
Robert-Abel Porras <majorping5@gmail.com>
2025-04-09 17:23:57 -0500

    Partial implementation of Mock Crime Scene

--- CSRS-Unity-Project/Assets/Modules/AE_New_York/Script/Door.cs
+++ CSRS-Unity-Project/Assets/Modules/AE_New_York/Script/Door.cs
@@ -3,21 +3,19 @@ using UnityEngine.UI;
 
 public class Door : MonoBehaviour
 {
-
-    bool trig, open;//trig-проверка входа выхода в триггер(игрок должен быть с тегом Player) open-закрыть и открыть дверь
+    bool trig, open;
     public float smooth = 2.0f;//скорость вращения
     public float DoorOpenAngle = 90.0f;//угол вращения 
     private Vector3 defaulRot;
     private Vector3 openRot;
-    public Text txt;//text 
-    // Start is called before the first frame update
+    public Text txt;
+    
     void Start()
     {
         defaulRot = transform.eulerAngles;
         openRot = new Vector3(defaulRot.x, defaulRot.y + DoorOpenAngle, defaulRot.z);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (open)//открыть
