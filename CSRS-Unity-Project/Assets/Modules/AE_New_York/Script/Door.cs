5616411b975977845373d2cc1536d22922e70196
Joseph Sailor Usher <sailorusher@outlook.com>
2025-04-15 12:17:07 -0500

    Fixed some assets

--- CSRS-Unity-Project/Assets/Modules/AE_New_York/Script/Door.cs
+++ CSRS-Unity-Project/Assets/Modules/AE_New_York/Script/Door.cs
@@ -3,19 +3,21 @@ using UnityEngine.UI;
 
 public class Door : MonoBehaviour
 {
-    bool trig, open;
+
+    bool trig, open;//trig-проверка входа выхода в триггер(игрок должен быть с тегом Player) open-закрыть и открыть дверь
     public float smooth = 2.0f;//скорость вращения
     public float DoorOpenAngle = 90.0f;//угол вращения 
     private Vector3 defaulRot;
     private Vector3 openRot;
-    public Text txt;
-    
+    public Text txt;//text 
+    // Start is called before the first frame update
     void Start()
     {
         defaulRot = transform.eulerAngles;
         openRot = new Vector3(defaulRot.x, defaulRot.y + DoorOpenAngle, defaulRot.z);
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (open)//открыть
@@ -26,8 +28,10 @@ public class Door : MonoBehaviour
         {
             transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, defaulRot, Time.deltaTime * smooth);
         }
-        // Replace this with new input system to make it work again.
-        // if (Input.GetKeyDown(KeyCode.E) && trig) open = !open;
+        if (Input.GetKeyDown(KeyCode.E) && trig)
+        {
+            open = !open;
+        }
         if (trig)
         {
             if (open)
